13a96402c32a33111973292b57765d1c44748704
Hugo Paiva <hugofrpalmeida@gmail.com>
2019-05-14 18:37:31 +0100

    Dossier

---
+++  Dossier (programa, slides e guiões práticos)/Práticas/Test.java
@@ -0,0 +1,17 @@
+package aula05;
+
+public class Test {
+	   public static void main(String[] args)  {
+		Aluno al = new Aluno ("Andreia Melo", 9855678,
+	      new Data(18, 7, 1990), new Data (1, 9, 2018));
+		Bolseiro bls = new Bolseiro ("Igor Santos", 8976543, new Data(11, 5, 1985));
+		bls.setBolsa(1050);
+		   	
+		System.out.println(“Aluno:” + al.getName());
+		System.out.println(al);
+		   	
+		System.out.println(“Bolseiro:” + bls.getName() + ", NMec: " 
+			+ bls.getNMec() + ", Bolsa:" +  bls.getBolsa());
+		System.out.println(bls);
+	   }
+} 
