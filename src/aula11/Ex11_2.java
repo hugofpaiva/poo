c3d29235428170bc8e35696135b8e7c8a3d2b7ba
Hugo Paiva <hugofrpalmeida@gmail.com>
2019-05-25 04:29:24 +0100

    Aula 11

--- src/aula11/Ex11_2.java
+++ src/aula11/Ex11_2.java
@@ -4,9 +4,19 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class Ex11_2 {
 
@@ -58,13 +68,13 @@ public class Ex11_2 {
 		} catch (FileNotFoundException e) {
 			System.out.println("O ficheiro não foi encontrado.");
 		}
-		//Tabela lista de Voos e média de Atrasos
+		// Tabela lista de Voos e média de Atrasos
 		HashMap<String, List<Integer>> companhias = new HashMap<>();
-		ArrayList<Integer> time = new ArrayList<>();
-		
-		HashMap<String, String> companhiasm = new HashMap<>();
+		TreeMap<String, Integer> companhiasm = new TreeMap<>();
+		HashMap<String, String> companhiasmtemp = new HashMap<>();
 		for (Voo i : voos) {
-			//System.out.println(i);
+			List<Integer> time = new ArrayList<>();
+			System.out.println(i);
 			String companhia = i.getCompanhia();
 			String Atraso = i.getAtraso();
 			String[] delay = Atraso.split(":");
@@ -77,37 +87,101 @@ public class Ex11_2 {
 				hourd = Integer.parseInt(delay[0]);
 				Integer ss = (hourd * 60 * 60) + (mind * 60);
 				if (companhias.containsKey(companhia)) {
-					ArrayList<Integer> timelist = (ArrayList<Integer>) companhias.get(companhia);
-					timelist.add(ss);
-					companhias.put(companhia, timelist);
+					time = companhias.get(companhia);
+					time.add(ss);
+					companhias.put(companhia, time);
 				} else {
 					time.add(ss);
 					companhias.put(companhia, time);
 				}
 			}
-			
-
-			
 		}
-		for(String l : companhias.keySet()) {
+		for (String l : companhias.keySet()) {
 			int total = 0;
-			for(int x : companhias.get(l)) {
-				total+=x;
+			for (int x : companhias.get(l)) {
+				total += x;
 			}
-			System.out.println(l+":"+total);
-			int media = total/companhias.get(l).size();
-			int min = (media/60)%60;
-			int hour = (media/60)/60;
+			int media = total / companhias.get(l).size();
+			int min = (media / 60) % 60;
+			int hour = (media / 60) / 60;
+			companhiasm.put(l, media);
+
+		}
+		Map<String, Integer> companhiasm1 = sortByComparator(companhiasm, "asc");
+		System.out.printf("%-20s%-12s\n", "Companhia", "Atraso Médio");
+		for (String m : companhiasm1.keySet()) {
+			int min = (companhiasm1.get(m) / 60) % 60;
+			int hour = (companhiasm1.get(m) / 60) / 60;
 			String smin = String.valueOf(min);
 			String shour = String.valueOf(hour);
-			String times = shour + ":" +smin;
-			companhiasm.put(l, times);
-			
+			if (min < 10) {
+				smin = "0" + smin;
+			}
+			if (hour < 10) {
+				shour = "0" + shour;
+			}
+			String times = shour + ":" + smin;
+			companhiasmtemp.put(m, times);
+			System.out.printf("%-20s%-12s\n", m, times);
+
 		}
-		System.out.println(companhiasm);
+		city(voos);
 
 	}
-	
-	
+
+	private static void city(ArrayList<Voo> vv) {
+		Map<String, Integer> Origens = new HashMap<>();
+		Map<String, Integer> Origensm = new HashMap<>();
+		int times = 0;
+		for (Voo i : vv) {
+			String Origem = i.getOrigem();
+
+			if (Origem.equals("Origem") || Origem.equals(" ")) {
+				continue;
+			} else {
+				if (Origens.containsKey(Origem)) {
+					times = Origens.get(Origem);
+					times += 1;
+					Origens.put(Origem, times);
+				} else {
+					times = 1;
+					Origens.put(Origem, times);
+				}
+			}
+		}
+		Origensm = sortByComparator(Origens, "desc");
+
+		try {
+			PrintWriter writer = new PrintWriter("cidades.txt");
+			writer.printf("%-22s%-12s\n", "Origem", "Voos");
+			for (String m : Origensm.keySet()) {
+				writer.printf("%-22s%-12d\n", m, Origensm.get(m));
+			}
+			writer.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("O ficheiro não foi encontrado.");
+		}
+
+	}
+
+	private static Map<String, Integer> sortByComparator(Map<String, Integer> unsortMap, String order) {
+		List<Entry<String, Integer>> list = new LinkedList<Entry<String, Integer>>(unsortMap.entrySet());
+		Collections.sort(list, new Comparator<Entry<String, Integer>>() {
+			public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {
+				if (order.equals("asc"))
+					return o1.getValue().compareTo(o2.getValue());
+				else if (order.equals("desc"))
+					return o2.getValue().compareTo(o1.getValue());
+				else
+					return 0;
+			}
+		});
+		Map<String, Integer> sortedMap = new LinkedHashMap<String, Integer>();
+		for (Entry<String, Integer> entry : list) {
+			sortedMap.put(entry.getKey(), entry.getValue());
+		}
+
+		return sortedMap;
+	}
 
 }
